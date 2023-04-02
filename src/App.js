a6301985bbee64ce2faa83aaf63830384d959d7a
shariful islam <sharifulb07@gmail.com>
2023-04-02 20:29:45 +0600

    Guest, packaginglist, propsPass and renderlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Todos from './Todos';
-import Gallery from './components/Gallery';
+import "./App.css";
+import Todos from "./Todos";
+import TodoList from "./components/TodoList";
+import { Profile } from "./components/propsPass/PropsTransfer";
+
+import PackagingList from "./components/packagingList/PackagingList";
+import RenderList from "./components/renderList/RenderList";
+import TeaCup from "./components/Guest/TeaCup";
 
 
 function App() {
   return (
     <div className="App">
-   <Gallery />
+      <TodoList />
+      <Profile />
+      <PackagingList />
+    <RenderList />
+
+    <TeaCup />
 
     </div>
   );
